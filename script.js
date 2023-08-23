06b4fe5079388317e1b6f9ff82ad41c8b193d51e
oleezy1 <ooyedemi@yahoo.com>
2023-08-22 21:28:11 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/*** navbar variables */
+const navOpenBtn = document.querySelector("[data-menu-open-btn]");
+const navCloseBtn = document.querySelector("[data-menu-close-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+for (let i = 0; i < navElemArr.length; i++) {
+    navElemArr[i].addEventListener("click", function () {
+
+        navbar.classList.toggle("active");
+        overlay.classList.toggle("active");
+        document.body.classList.toggle("active");
+    });
+}
+
+
+/*** navbar variables */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+    window.scrollY ≥ 10 ? header.classList.add("active") : header.classList.remove("active");
+
+});
+
+
+/*** GO TOP */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+window.addEventListener("scroll", function () {
+    this.window.scrollY ≥ 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active);
+
+});
